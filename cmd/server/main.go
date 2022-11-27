659aea5935bf63837edb78bb2dbd2e32fc391a5e
Dustin Gilmore <21026373+gilmored@users.noreply.github.com>
2022-11-26 18:19:18 -0600

    added product db

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"net/http"
 	"os"
@@ -18,14 +20,88 @@ type product struct {
 
 var products []product
 
-func initProducts() {
-	bs, err := os.ReadFile("products.json")
+func createDB() {
+	os.Remove("./products.db")
+	db, err := sql.Open("sqlite3", "./products.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	sqlStmt := `
+		CREATE TABLE products_table (ID INTEGER NOT NULL PRIMARY KEY, name TEXT, description TEXT, price REAL);`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return
+	}
+
+	bs, err := os.ReadFile("./products.json")
 	if err != nil {
 		log.Fatal(err)
 	}
 	if err := json.Unmarshal(bs, &products); err != nil {
 		log.Fatal(err)
 	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	stmt, err := tx.Prepare("INSERT INTO products_table(id, name, description, price) VALUES(?, ?, ?, ?)")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer stmt.Close()
+
+	for _, prod := range products {
+		_, err = stmt.Exec(prod.ID, prod.Name, prod.Description, prod.Price)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	rows, err := db.Query("SELECT id, name, description, price FROM products_table LIMIT 5")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	type prodDB struct {
+		id          int
+		name        string
+		description string
+		price       int64
+	}
+	var prods []prodDB
+	for rows.Next() {
+		var p prodDB
+		if err := rows.Scan(&p.id, &p.name, &p.description, &p.price); err != nil {
+			log.Fatal(err)
+		}
+		prods = append(prods, p)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+	status := "status: up"
+	if err := db.Ping(); err != nil {
+		status = "status: down"
+	}
+	log.Println(status)
+	log.Println(prods)
+	return
+
 }
 
 func getProducts(w http.ResponseWriter, r *http.Request) {
@@ -107,7 +183,7 @@ func deleteProduct(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	initProducts()
+	createDB()
 	r := mux.NewRouter()
 
 	r.HandleFunc("/api/products", getProducts).Methods("GET")
