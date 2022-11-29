eb62e43d2af269c712323894f282a7f5f3be02a1
Dustin Gilmore <21026373+gilmored@users.noreply.github.com>
2022-11-28 22:51:43 -0600

    added POW function and testing to calculator

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -8,15 +8,18 @@ import (
 func TestCalculator(t *testing.T) {
 	tests := map[string]struct {
 		a, b, expect int
+		x, y, want   float64
 		err          string
 		f            func(int, int) int
 		fEr          func(int, int) (int, error)
+		testPow      func(float64, float64) float64
 	}{
 		"Add":          {a: 2, b: 1, expect: 3, f: calculator.Add},
 		"Subtract":     {a: 2, b: 1, expect: 1, f: calculator.Subtract},
 		"Multiply":     {a: 2, b: 2, expect: 4, f: calculator.Multiply},
 		"Divide":       {a: 4, b: 2, expect: 2, fEr: calculator.Divide},
 		"DivideByZero": {a: 2, b: 0, expect: 0, fEr: calculator.Divide},
+		"Power":        {x: 2, y: 3, want: 8, testPow: calculator.Power},
 	}
 
 	for name, test := range tests {
@@ -34,6 +37,12 @@ func TestCalculator(t *testing.T) {
 					t.Errorf("got %d, expected %d", result, test.expect)
 				}
 				return
+			}
+			if test.testPow != nil {
+				result := test.testPow(test.x, test.y)
+				if result != test.want {
+					t.Errorf("got %v, expected %v", result, test.want)
+				}
 			}
 		})
 	}
