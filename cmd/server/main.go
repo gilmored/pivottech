9e76819e3ad5d2c4fde97bbaa08eee844505dbe1
Dustin Gilmore <21026373+gilmored@users.noreply.github.com>
2022-11-30 20:48:09 -0600

    added db connection to server. Working on getProducts handler.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,110 +7,101 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"net/http"
-	"os"
 	"strconv"
 )
 
 type product struct {
-	ID          int    `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Price       int    `json:"price"`
+	ID          int     `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
 }
 
 var products []product
 
-func createDB() {
-	os.Remove("./products.db")
-	db, err := sql.Open("sqlite3", "./products.db")
+//type prodDB struct {
+//	id          int
+//	name        string
+//	description string
+//	price       int64
+//}
+//
+//var prods []prodDB
+
+var db *sql.DB
+var err error
+
+func connectDB() *sql.DB {
+	db, err = sql.Open("sqlite3", "/Users/dustin/go/src/github.com/gilmored/pivottech/cmd/seeder/products.db")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
+	//defer db.Close()
 
-	sqlStmt := `
-		CREATE TABLE products_table (ID INTEGER NOT NULL PRIMARY KEY, name TEXT, description TEXT, price REAL);`
-	_, err = db.Exec(sqlStmt)
-	if err != nil {
-		log.Printf("%q: %s\n", err, sqlStmt)
-		return
-	}
-
-	bs, err := os.ReadFile("./products.json")
+	rows, err := db.Query("SELECT id, name, description, price FROM products_table")
 	if err != nil {
 		log.Fatal(err)
 	}
-	if err := json.Unmarshal(bs, &products); err != nil {
-		log.Fatal(err)
-	}
+	//defer rows.Close()
 
-	tx, err := db.Begin()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	stmt, err := tx.Prepare("INSERT INTO products_table(id, name, description, price) VALUES(?, ?, ?, ?)")
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer stmt.Close()
-
-	for _, prod := range products {
-		_, err = stmt.Exec(prod.ID, prod.Name, prod.Description, prod.Price)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	err = tx.Commit()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	rows, err := db.Query("SELECT id, name, description, price FROM products_table LIMIT 5")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-
-	type prodDB struct {
-		id          int
-		name        string
-		description string
-		price       int64
-	}
-	var prods []prodDB
 	for rows.Next() {
-		var p prodDB
-		if err := rows.Scan(&p.id, &p.name, &p.description, &p.price); err != nil {
+		var id int
+		var name string
+		var description string
+		var price float64
+		if err := rows.Scan(&id, &name, &description, &price); err != nil {
 			log.Fatal(err)
 		}
-		prods = append(prods, p)
+		products = append(products, product{ID: id, Name: name, Description: description, Price: price})
 	}
 
-	err = rows.Err()
-	if err != nil {
-		log.Fatal(err)
-	}
+	//err = rows.Err()
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+
 	status := "status: up"
 	if err := db.Ping(); err != nil {
 		status = "status: down"
+		log.Fatalf("%s error: %v", status, err)
 	}
 	log.Println(status)
-	log.Println(prods)
-	return
-
+	log.Println(products)
+	return db
 }
 
 func getProducts(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(products); err != nil {
+	//w.Header().Set("Content-Type", "application/json")
+	///
+	//vars := mux.Vars(r)
+	//limit, err := strconv.ParseInt(vars["limit"], 10, 64)
+	//
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//if limit < 0 || limit > int64(len(products)) {
+	//	total := len(products)
+	//	log.Printf("Selected limit is not within Products range. Please select a limit between 1 and %d", total)
+	//} else {
+	//	if _, err = db.Query("SELECT * FROM products_table LIMIT ?", limit); err != nil {
+	//		log.Println("error", err)
+	//		w.WriteHeader(http.StatusInternalServerError)
+	//	}
+	//}
+	if _, err = db.Query("SELECT * FROM products_table"); err != nil {
 		log.Println("error", err)
 		w.WriteHeader(http.StatusInternalServerError)
 	}
 }
+
+//func getProducts(w http.ResponseWriter, r *http.Request) {
+//	w.Header().Set("Content-Type", "application/json")
+//	if err := json.NewEncoder(w).Encode(products); err != nil {
+//		log.Println("error", err)
+//		w.WriteHeader(http.StatusInternalServerError)
+//	}
+//}
 
 func getProduct(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -183,15 +174,16 @@ func deleteProduct(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	createDB()
+	connectDB()
 	r := mux.NewRouter()
-
+	//r.HandleFunc("/api/products?limit={limit}", getProducts).Methods("GET")
+	//
 	r.HandleFunc("/api/products", getProducts).Methods("GET")
-	r.HandleFunc("/api/products/{id}", getProduct).Methods("GET")
-	r.HandleFunc("/api/products", createProduct).Methods("POST")
-	r.HandleFunc("/api/products/{id}", updateProduct).Methods("PUT")
-	r.HandleFunc("/api/products/{id}", deleteProduct).Methods("DELETE")
-
+	//r.HandleFunc("/api/products/{id}", getProduct).Methods("GET")
+	//r.HandleFunc("/api/products", createProduct).Methods("POST")
+	//r.HandleFunc("/api/products/{id}", updateProduct).Methods("PUT")
+	//r.HandleFunc("/api/products/{id}", deleteProduct).Methods("DELETE")
+	//
 	log.Println("Listening on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
