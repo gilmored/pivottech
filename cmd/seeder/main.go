5fe3c906d8c94ff81aa941b51c702b13301312ae
Dustin Gilmore <21026373+gilmored@users.noreply.github.com>
2022-12-03 22:04:23 -0600

    updated seeder

--- cmd/seeder/main.go
+++ cmd/seeder/main.go
@@ -9,44 +9,35 @@ import (
 )
 
 type product struct {
-	ID          int    `json:"id"`
+	Id          int    `json:"id"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
-	Price       int    `json:"price"`
+	Price       int64  `json:"price"`
 }
-
-var products []product
-
-type prodDB struct {
-	id          int
-	name        string
-	description string
-	price       int64
-}
-
-var prods []prodDB
 
 func main() {
 	os.Remove("./products.db")
-	db, err := sql.Open("sqlite3", "./products.db")
+
+	db, err := sql.Open("sqlite3", "products.db")
+
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	sqlStmt := `
-		CREATE TABLE products_table (ID INTEGER NOT NULL PRIMARY KEY, name TEXT, description TEXT, price REAL);`
+	sqlStmt := `CREATE TABLE products_table (ID INTEGER NOT NULL PRIMARY KEY, name TEXT, description TEXT, price REAL);`
 	_, err = db.Exec(sqlStmt)
 	if err != nil {
 		log.Printf("%q: %s\n", err, sqlStmt)
 		return
 	}
-
+	var products []product
 	bs, err := os.ReadFile("./products.json")
 	if err != nil {
 		log.Fatal(err)
 	}
 	if err := json.Unmarshal(bs, &products); err != nil {
+
 		log.Fatal(err)
 	}
 
@@ -61,9 +52,8 @@ func main() {
 		return
 	}
 	defer stmt.Close()
-
 	for _, prod := range products {
-		_, err = stmt.Exec(prod.ID, prod.Name, prod.Description, prod.Price)
+		_, err = stmt.Exec(prod.Id, prod.Name, prod.Description, prod.Price)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -82,11 +72,11 @@ func main() {
 	defer rows.Close()
 
 	for rows.Next() {
-		var p prodDB
-		if err := rows.Scan(&p.id, &p.name, &p.description, &p.price); err != nil {
+		var p product
+		if err := rows.Scan(&p.Id, &p.Name, &p.Description, &p.Price); err != nil {
 			log.Fatal(err)
 		}
-		prods = append(prods, p)
+		products = append(products, p)
 	}
 
 	err = rows.Err()
@@ -98,7 +88,7 @@ func main() {
 		status = "status: down"
 	}
 	log.Println(status)
-	log.Println(prods)
+	log.Println(products)
 	return
 
 }
